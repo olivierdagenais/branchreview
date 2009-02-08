c6ead53fc5656e917999f63a3a24af63074f7431
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2009-02-08 20:12:06 +0000

    Initial .NET 2.0 console application skeleton.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@2 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ BranchAndReviewTools/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace SoftwareNinjas.BranchAndReviewTools
+{
+    static class Extensions
+    {
+        public static T GetCustomAttribute<T>(this Assembly assembly) where T : Attribute
+        {
+            object[] attributes = assembly.GetCustomAttributes(typeof(T), false);
+            
+            return ( (T) attributes[0] );
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var self = Assembly.GetExecutingAssembly ( );
+            string product = self.GetCustomAttribute<AssemblyProductAttribute> ( ).Product;
+            string copyright = self.GetCustomAttribute<AssemblyCopyrightAttribute> ( ).Copyright;
+            string version = self.GetName ( ).Version.ToString ( );
+
+            Console.WriteLine ( "{0} version {1} - {2}", product, version, copyright );
+        }
+    }
+}
