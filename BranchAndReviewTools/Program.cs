273b03137d412ff75aa2df18ba42a383ac7d1c6a
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2009-02-10 04:00:17 +0000

    1 - Added NAnt 0.85 without documentation and with a slight modification to include support for .NET 3.5
    2 - Added NAntContrib 0.85 without documentation and with a schema generated by the <nantschema> task.
    
    3 - The main project now references NUnit and added a token unit test.
    
    4 - Added a minimal NAnt build script that cleans, compiles and runs tests.
    
    
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@3 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

--- BranchAndReviewTools/Program.cs
+++ BranchAndReviewTools/Program.cs
@@ -18,14 +18,20 @@ namespace SoftwareNinjas.BranchAndReviewTools
 
     class Program
     {
+        internal static string GenerateHeader(Assembly source)
+        {
+            string product = source.GetCustomAttribute<AssemblyProductAttribute> ( ).Product;
+            string copyright = source.GetCustomAttribute<AssemblyCopyrightAttribute> ( ).Copyright;
+            string version = source.GetName ( ).Version.ToString ( );
+
+            string result = String.Format ( "{0} version {1} - {2}", product, version, copyright );
+            return result;
+        }
+
         static void Main(string[] args)
         {
             var self = Assembly.GetExecutingAssembly ( );
-            string product = self.GetCustomAttribute<AssemblyProductAttribute> ( ).Product;
-            string copyright = self.GetCustomAttribute<AssemblyCopyrightAttribute> ( ).Copyright;
-            string version = self.GetName ( ).Version.ToString ( );
-
-            Console.WriteLine ( "{0} version {1} - {2}", product, version, copyright );
+            Console.WriteLine ( GenerateHeader(self) );
         }
     }
 }
