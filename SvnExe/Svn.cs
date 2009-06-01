79ef4dd16d223fcb9a2843c1315836a5b89924bf
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2009-06-01 02:45:38 +0000

    Implemented the SvnExe base set-up code that tries to detect a configuration and otherwise extracts the ZIP contained within the assembly to a non-roaming folder, saving a new configuration file in the process.  This will ensure the user won't have to install and keep Subversion binaries in the path if they don't want to and provides a self-healing mechanism for access to the Subversion client binaries.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@15 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ SvnExe/Svn.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.IO;
+
+using SoftwareNinjas.BranchAndReviewTools.Core;
+using System.Configuration;
+using System.Globalization;
+using System.Reflection;
+using System.Security.AccessControl;
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace SoftwareNinjas.BranchAndReviewTools.SvnExe
+{
+    internal class Svn
+    {
+        public const int UnzipBlockSize = 2048;
+        public static readonly string ConfigFile = Assembly.GetCallingAssembly().GetName().Name + ".dll.config";
+        public static readonly Version CurrentVersion = new Version(1, 5, 6);
+        private string _pathToExecutable = null;
+
+        public Svn() : this(CurrentVersion, ConfigFolderPath)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes an instance of the sub-process-based Subversion client system.
+        /// </summary>
+        /// 
+        /// <param name="requestedVersion">
+        /// The <see cref="Version"/> representing the Subversion release to look for.
+        /// </param>
+        /// 
+        /// <param name="configFolderPath">
+        /// The absolute path to a local (non-roaming) folder where the configuration file and optionally the Subversion
+        /// binaries will be found.
+        /// </param>
+        public Svn(Version requestedVersion, string configFolderPath)
+        {
+            var versionStamp = DetermineSubversionVersionStamp(requestedVersion);
+            _pathToExecutable = CheckConfiguration(versionStamp, configFolderPath);
+
+            if (null == _pathToExecutable)
+            {
+                // TODO: scan PATH environment variable for binaries for this version
+                // TODO: if found, update config
+            }
+
+            if (null == _pathToExecutable)
+            {
+                // TODO: check for evidence of installed Subversion binaries (MSI, APT, YUM, etc.) for this version
+                // TODO: if found, update config
+            }
+
+            if (null == _pathToExecutable)
+            {
+                _pathToExecutable = ExtractSubversionBinaries(configFolderPath, versionStamp);
+                SaveConfiguration(configFolderPath, versionStamp, _pathToExecutable);
+            }
+        }
+
+        internal string PathToExecutable
+        {
+            get
+            {
+                return _pathToExecutable;
+            }
+        }
+
+        internal static string ExtractSubversionBinaries(string configFolderPath, string versionStamp)
+        {
+            string result = null;
+            var me = Assembly.GetCallingAssembly();
+            var sourcePath = String.Format(CultureInfo.InvariantCulture, "Resources.{0}.zip", versionStamp);
+            using (var ins = me.GetManifestResourceStream(typeof(Svn), sourcePath))
+            {
+                ExtractZip(ins, configFolderPath, (entry) => {
+                    if (Path.GetFileName(entry.Name) == "svn.exe")
+                    {
+                        result = Path.Combine(configFolderPath, entry.Name);
+                    }
+                });
+            }
+            return result;
+        }
+
+        internal static void ExtractZip(Stream inputStream, string targetFolderPath, Action<ZipEntry> progress)
+        {
+            byte[] data = new byte[UnzipBlockSize];
+            using (var zis = new ZipInputStream(inputStream))
+            {
+                ZipEntry entry;
+                while (( entry = zis.GetNextEntry() ) != null)
+                {
+                    var fullPath = Path.Combine(targetFolderPath, entry.Name);
+                    if (progress != null)
+                    {
+                        progress(entry);
+                    }
+                    if (entry.IsDirectory)
+                    {
+                        Directory.CreateDirectory(fullPath);
+                    }
+                    else if (entry.IsFile)
+                    {
+                        var dirName = Path.GetDirectoryName(fullPath);
+                        Directory.CreateDirectory(dirName);
+                        using (var os = File.Open(fullPath, FileMode.Create, FileAccess.Write, FileShare.Read))
+                        {
+                            int actuallyRead = 0;
+                            while (( actuallyRead = zis.Read(data, 0, UnzipBlockSize) ) != 0)
+                            {
+                                os.Write(data, 0, actuallyRead);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        internal static void SaveConfiguration(string configFolderPath, string versionStamp, string pathToExecutable)
+        {
+            Directory.CreateDirectory(configFolderPath);
+
+            var pathToConfigFile = Path.Combine(configFolderPath, ConfigFile);
+            using (var sw = new StreamWriter(pathToConfigFile))
+            {
+                var me = Assembly.GetCallingAssembly();
+                using (var ins = me.GetManifestResourceStream(typeof(Svn), "Resources.template.config"))
+                {
+                    using (var sr = new StreamReader(ins))
+                    {
+                        string line = null;
+                        while((line = sr.ReadLine()) != null)
+                        {
+                            sw.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            var config = LoadConfiguration(pathToConfigFile);
+            var element = config.AppSettings.Settings[versionStamp];
+            element.Value = pathToExecutable;
+            config.Save();
+        }
+
+        /// <summary>
+        /// Makes use of the .NET <see cref="System.Configuration"/> API to load an arbitrary XML configuration file
+        /// without having to parse the XML ourselves.
+        /// </summary>
+        /// 
+        /// <param name="pathToConfigFile">
+        /// The full path to the <c>.config</c> file.
+        /// </param>
+        /// 
+        /// <returns>
+        /// A <see cref="Configuration"/> instance, even if the file at the other end is corrupted!
+        /// </returns>
+        internal static Configuration LoadConfiguration(string pathToConfigFile)
+        {
+            var configMap = new ExeConfigurationFileMap();
+            configMap.ExeConfigFilename = pathToConfigFile;
+            Configuration config = 
+                ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+            return config;
+        }
+
+        /// <summary>
+        /// If there is a configuration file inside <paramref name="configFolderPath"/>, it's valid, it contains an
+        /// entry for the specified <paramref name="versionStamp"/> and that entry exists, then returns said entry;
+        /// otherwise <see langword="null"/>.
+        /// </summary>
+        /// 
+        /// <param name="versionStamp">
+        /// A version string commonly used to identify Subversion releases.
+        /// </param>
+        /// 
+        /// <param name="configFolderPath">
+        /// The absolute path to the folder to look into for the <c>.config</c> file.
+        /// </param>
+        /// 
+        /// <returns>
+        /// The path to a subversion binary if found; <see langword="null"/> otherwise.
+        /// </returns>
+        internal static string CheckConfiguration(string versionStamp, string configFolderPath)
+        {
+            string result = null;
+            var pathToConfigFile = Path.Combine(configFolderPath, ConfigFile);
+            if (File.Exists(pathToConfigFile))
+            {
+                var config = LoadConfiguration(pathToConfigFile);
+                if (config != null)
+                {
+                    if (config.Sections["appSettings"] is AppSettingsSection)
+                    {
+                        var add = config.AppSettings.Settings[versionStamp];
+                        if (add != null && add.Value != null)
+                        {
+                            var potentialValue = add.Value.Trim();
+                            if (File.Exists(potentialValue))
+                            {
+                                result = potentialValue;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static string DetermineSubversionVersionStamp(Version requestedVersion)
+        {
+            // TODO: determine this from Environment or something
+            string platform = "win32";
+            string result = String.Format(CultureInfo.InvariantCulture, "svn-{0}-{1}.{2}.{3}", 
+                platform, requestedVersion.Major, requestedVersion.Minor, requestedVersion.Build);
+            return result;
+        }
+
+        public static string ConfigFolderPath
+        {
+            get
+            {
+                // Windows 2000 & XP:  C:\Documents and Settings\Oli\Local Settings\Application Data\
+                // Windows Vista & 7:  C:\Users\Oli\AppData\Local\
+                var localData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                var result = Path.Combine(localData, "Software Ninjas/Branch And Review Tools/");
+                return result;
+            }
+        }
+    }
+}
---
+++ SvnExe/Test/Svn.cs
@@ -0,0 +1,206 @@
+﻿using System;
+
+using Parent = SoftwareNinjas.BranchAndReviewTools.SvnExe;
+using NUnit.Framework;
+using System.IO;
+
+namespace SoftwareNinjas.BranchAndReviewTools.SvnExe.Test
+{
+    /// <summary>
+    /// A class to test <see cref="Parent.Svn"/>.
+    /// </summary>
+    [TestFixture]
+    public class Svn
+    {
+        private string _inputFolder;
+        private string _outputFolder;
+        private string _versionStamp;
+
+        /// <summary>
+        /// Initializes a new instance for use with some short-lived files.
+        /// </summary>
+        public Svn()
+        {
+            _inputFolder = Path.Combine(Environment.CurrentDirectory, "../../../SvnExe/Test");
+            _outputFolder = Path.Combine(Environment.CurrentDirectory, "../../../TestOutput");
+            _versionStamp = Parent.Svn.DetermineSubversionVersionStamp(Parent.Svn.CurrentVersion);
+        }
+
+        /// <summary>
+        /// Erases and re-creates output folder.
+        /// </summary>
+        [SetUp]
+        public void PrepareOutputFolder()
+        {
+            ClearOutputFolder();
+            Directory.CreateDirectory(_outputFolder);
+        }
+
+        /// <summary>
+        /// Erases the files from the output folder.
+        /// </summary>
+        [TearDown]
+        public void ClearOutputFolder()
+        {
+            if (Directory.Exists(_outputFolder))
+            {
+                Directory.Delete(_outputFolder, true);
+            }
+        }
+
+        /// <summary>
+        /// Convenience method to obtain the path to a folder in which files may be written to for testing purposes.
+        /// </summary>
+        /// 
+        /// <param name="subFolderName">
+        /// The name of a sub-folder that identifies the test scenario.
+        /// </param>
+        /// 
+        /// <returns>
+        /// The full path to a folder in which it is safe to create files.
+        /// </returns>
+        public string GetOutputFolderPath(string subFolderName)
+        {
+            string result = Path.Combine(_outputFolder, subFolderName);
+            return result;
+        }
+
+        /// <summary>
+        /// Creates a sub-folder named after the provided <paramref name="scenario"/> and copies the config file that
+        /// was in the folder of the same name in the source tree as a base.
+        /// </summary>
+        /// 
+        /// <param name="scenario">
+        /// A short string identifying the test condition being exercised.
+        /// </param>
+        /// 
+        /// <returns>
+        /// The full path to the recently-created folder.
+        /// </returns>
+        public string PrepareTestFile(string scenario)
+        {
+            var sub = GetOutputFolderPath(scenario);
+            Directory.CreateDirectory(sub);
+            File.Copy(
+                Path.Combine(_inputFolder, scenario + "/" + Parent.Svn.ConfigFile),
+                sub + "/" + Parent.Svn.ConfigFile);
+            string result = sub;
+            return result;
+        }
+
+        /// <summary>
+        /// Verifies the string formatting.
+        /// </summary>
+        [Test]
+        public void DetermineSubversionVersionStamp()
+        {
+            Assert.AreEqual("svn-win32-1.5.6", Parent.Svn.DetermineSubversionVersionStamp(new Version(1, 5, 6)));
+        }
+
+        /// <summary>
+        /// Should not fail on first run.
+        /// </summary>
+        [Test]
+        public void CheckConfiguration_FolderDoesNotExist()
+        {
+            Assert.AreEqual(null, Parent.Svn.CheckConfiguration(null, GetOutputFolderPath("FolderDoesNotExist")));
+        }
+
+        /// <summary>
+        /// Should not fail even if user deleted file but not folders.
+        /// </summary>
+        [Test]
+        public void CheckConfiguration_FileDoesNotExist()
+        {
+            var sub = GetOutputFolderPath("FileDoesNotExist");
+            Directory.CreateDirectory(sub);
+            Assert.AreEqual(null, Parent.Svn.CheckConfiguration(null, sub));
+        }
+
+        /// <summary>
+        /// Should not fail even if user removed the entry we're looking for.
+        /// </summary>
+        [Test]
+        public void CheckConfiguration_NoEntryInFile()
+        {
+            Assert.AreEqual(
+                null,
+                Parent.Svn.CheckConfiguration(_versionStamp, PrepareTestFile("NoEntryInFile")));
+        }
+
+        /// <summary>
+        /// Should not fail even if the configuration is outdated.
+        /// </summary>
+        [Test]
+        public void CheckConfiguration_EntryPointsToInvalidLocation()
+        {
+            Assert.AreEqual(
+                null,
+                Parent.Svn.CheckConfiguration(_versionStamp, PrepareTestFile("EntryPointsToInvalidLocation")));
+        }
+
+        /// <summary>
+        /// Manual/integration test that relies on the developer (i.e. you who is reading this right now) already having
+        /// the config file where it would normally reside.
+        /// </summary>
+        public void CheckConfiguration_EntryPointsToValidLocation()
+        {
+            string result = Parent.Svn.CheckConfiguration(_versionStamp, Parent.Svn.ConfigFolderPath);
+            Assert.IsNotNull(result);
+            Assert.IsTrue(File.Exists(result));
+        }
+
+        /// <summary>
+        /// Verifies the config can be saved and re-read.
+        /// </summary>
+        [Test]
+        public void SaveConfiguration()
+        {
+            var sub = GetOutputFolderPath("SaveConfiguration");
+            // recursive test is recursive (...and records the location of its own file)
+            var expected = Path.Combine(sub, Parent.Svn.ConfigFile);
+            Parent.Svn.SaveConfiguration(sub, _versionStamp, expected);
+            string actual = Parent.Svn.CheckConfiguration(_versionStamp, sub);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Verifies the from-Assembly ZIP extraction.
+        /// </summary>
+        [Test]
+        public void ExtractSubversionBinaries()
+        {
+            var sub = GetOutputFolderPath("ExtractSubversionBinaries");
+            var pathToBin = Parent.Svn.ExtractSubversionBinaries(sub, _versionStamp);
+            Assert.IsTrue(Directory.Exists(Path.Combine(sub, _versionStamp)));
+            Assert.IsTrue(File.Exists(pathToBin));
+            Assert.AreEqual("svn.exe", Path.GetFileName(pathToBin));
+        }
+
+        /// <summary>
+        /// Overall test of the glue that connects all the previously-tested parts.
+        /// </summary>
+        [Test]
+        public void Integration()
+        {
+            var sub = GetOutputFolderPath("Integration");
+
+            var svn = new Parent.Svn(Parent.Svn.CurrentVersion, sub);
+            var actualBinaryPath = svn.PathToExecutable;
+
+            Assert.IsTrue(File.Exists(actualBinaryPath));
+
+            var folder = Path.GetDirectoryName(actualBinaryPath);
+            var greatGrandParent = Path.Combine(folder, "../..");
+            var configFile = Path.Combine(greatGrandParent, Parent.Svn.ConfigFile);
+            Assert.IsTrue(File.Exists(configFile));
+            string entireFile;
+            using (var sr = new StreamReader(configFile))
+            {
+                entireFile = sr.ReadToEnd();
+            }
+            Assert.IsTrue(entireFile.Contains(actualBinaryPath));
+            Assert.IsTrue(entireFile.Contains(_versionStamp));
+        }
+    }
+}
