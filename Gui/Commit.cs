daa04587930d7e2da974f9f8742882fe38876a9e
olivier.dagenais@gmail.com <olivier.dagenais@gmail.com@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2010-12-02 15:41:30 +0000

    Initial creation of a Gui project with a skeleton for the Commit window.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@25 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui/Commit.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui
+{
+    public partial class Commit : Form
+    {
+        public Commit()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gui/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SoftwareNinjas.BranchAndReviewTools.Gui")]
+[assembly: AssemblyDescription("Graphical User Interface for the Branch And Review Tools")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Software Ninjas")]
+[assembly: AssemblyProduct("Branch And Review Tools (BART)")]
+[assembly: AssemblyCopyright("(C) Copyright 2010 Software Ninjas")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4888f359-e94b-4cfe-9b1a-8efade6ff271")]
