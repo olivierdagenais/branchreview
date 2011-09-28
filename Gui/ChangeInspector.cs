508eec877da98f39b798288aa4074c7b646a8bc8
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2011-09-28 02:23:44 +0000

    Added some properties to represent the callbacks the ChangeInspector needs.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@188 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

--- Gui/ChangeInspector.cs
+++ Gui/ChangeInspector.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using SoftwareNinjas.BranchAndReviewTools.Core;
 
 namespace SoftwareNinjas.BranchAndReviewTools.Gui
 {
@@ -18,8 +22,8 @@ namespace SoftwareNinjas.BranchAndReviewTools.Gui
             set { PatchViewer.SetReadOnlyText(value); }
         }
 
-        // TODO: GetPendingChanges
-        // TODO: ComputeDifferences
-        // TODO: GetActionsForPendingChanges
+        public Func<object, DataTable> PendingChangesFunction { get; set; }
+        public Func<IEnumerable<object>, string> ComputeDifferencesFunction { get; set; }
+        public Func<IEnumerable<object>, IList<MenuAction>> ActionsForPendingChangesFunction { get; set; }
     }
 }
