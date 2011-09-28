fba756efd2658cffe95d6c8784a27803fe090439
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2011-09-28 02:42:42 +0000

    Extracted a Matches() overload for one chunk. Also renamed a local variable for clarity.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@250 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

--- Gui/Extensions/DataRowExtensions.cs
+++ Gui/Extensions/DataRowExtensions.cs
@@ -10,39 +10,45 @@ namespace SoftwareNinjas.BranchAndReviewTools.Gui.Extensions
         {
             foreach (var chunk in filterChunks)
             {
-                bool chunkMatchesColumn = false;
-                for (var c = 0; c < columnTypes.Count; c++)
-                {
-                    var columnType = columnTypes[c];
-                    var cellValue = row[c];
+                var chunkMatchesAtLeastOneColumn = Matches(row, columnTypes, chunk);
 
-                    if (columnType == typeof(int))
-                    {
-                        if (chunk.Integer.HasValue)
-                        {
-                            var v = (int) cellValue;
-                            chunkMatchesColumn = v == chunk.Integer.Value;
-                        }
-                    }
-                    else
-                    {
-                        if (cellValue != null)
-                        {
-                            var v = cellValue.ToString();
-                            chunkMatchesColumn = v.ContainsInvariantIgnoreCase(chunk.Text);
-                        }
-                    }
-
-                    if (chunkMatchesColumn)
-                    {
-                        break;
-                    }
-                }
-
-                if (!chunkMatchesColumn) return false;
+                if (!chunkMatchesAtLeastOneColumn) return false;
             }
 
             return true;
+        }
+
+        public static bool Matches(this DataRow row, IList<Type> columnTypes, FilterChunk chunk)
+        {
+            var chunkMatchesAtLeastOneColumn = false;
+            for (var c = 0; c < columnTypes.Count; c++)
+            {
+                var columnType = columnTypes[c];
+                var cellValue = row[c];
+
+                if (columnType == typeof(int))
+                {
+                    if (chunk.Integer.HasValue)
+                    {
+                        var v = (int) cellValue;
+                        chunkMatchesAtLeastOneColumn = v == chunk.Integer.Value;
+                    }
+                }
+                else
+                {
+                    if (cellValue != null)
+                    {
+                        var v = cellValue.ToString();
+                        chunkMatchesAtLeastOneColumn = v.ContainsInvariantIgnoreCase(chunk.Text);
+                    }
+                }
+
+                if (chunkMatchesAtLeastOneColumn)
+                {
+                    break;
+                }
+            }
+            return chunkMatchesAtLeastOneColumn;
         }
     }
 }
