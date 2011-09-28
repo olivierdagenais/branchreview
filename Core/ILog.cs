ee936c27470418724c2931bf95ffbfe44e7cc5d4
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2011-09-28 02:40:07 +0000

    Introducing the ILog interface.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@242 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Core/ILog.cs
@@ -0,0 +1,53 @@
+﻿namespace SoftwareNinjas.BranchAndReviewTools.Core
+{
+    /// <summary>
+    /// Represents the mechanism used for reporting status to the host.
+    /// </summary>
+    public interface ILog
+    {
+        /// <summary>
+        /// Reports status with the specified <paramref name="message"/>.
+        /// </summary>
+        /// 
+        /// <param name="message">
+        /// The status to show the user.
+        /// </param>
+        void Info(string message);
+
+        /// <summary>
+        /// Reports status with the specified <paramref name="message"/>
+        /// and progress with the specified <paramref name="progressValue"/> and <paramref name="progressMaximum"/>.
+        /// </summary>
+        /// 
+        /// <param name="message">
+        /// The status to show the user.
+        /// </param>
+        /// 
+        /// <param name="progressValue">
+        /// An integer representing the progress toward <paramref name="progressMaximum"/>.
+        /// </param>
+        /// 
+        /// <param name="progressMaximum">
+        /// An integer representing the maximum value of <paramref name="progressValue"/>.
+        /// </param>
+        void Info(string message, int progressValue, int progressMaximum);
+
+        /// <summary>
+        /// Reports a warning with the specified <paramref name="message"/>.
+        /// </summary>
+        /// 
+        /// <param name="message">
+        /// The warning to show the user.
+        /// </param>
+        void Warning(string message);
+
+        /// <summary>
+        /// Reports an error with the specified <paramref name="message"/>.
+        /// </summary>
+        /// 
+        /// <param name="message">
+        /// The error to show the user.
+        /// </param>
+        void Error(string message);
+    }
+}
