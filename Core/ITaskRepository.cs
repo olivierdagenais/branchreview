7c594d0623c17529543ad623b371d62eee164507
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2011-09-28 01:59:44 +0000

    Introducing ITaskRepository and a mock implementation for testing.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@99 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Core/ITaskRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Core
+{
+    /// <summary>
+    /// Represents a system that keeps track of work items, cases, bugs, features, tasks, user stories, etc.
+    /// </summary>
+    public interface ITaskRepository
+    {
+        /// <summary>
+        /// Loads a <see cref="DataTable"/> representing the available tasks.  Pre-filtering is assumed to have taken
+        /// place.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// A <see cref="DataTable"/> with at least the following columns:
+        /// <list type="table">
+        ///     <listheader><term>Column name</term><description>Description</description></listheader>
+        ///     <item><term>ID</term><description>A unique identifier for tasks</description></item>
+        ///     <item><term>Title</term><description>A summary of the work to be performed</description></item>
+        /// </list>
+        /// </returns>
+        DataTable LoadTasks();
+    }
+}
---
+++ Gui/Mock/TaskRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using SoftwareNinjas.BranchAndReviewTools.Core;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Mock
+{
+    class TaskRepository : ITaskRepository
+    {
+        private readonly DataTable _tasks = new DataTable
+        {
+            Columns =
+            {
+                new DataColumn("ID", typeof(int)) { Caption = "Case" },
+                {"Type", typeof(string)},
+                {"Title", typeof(string)},
+                {"Status", typeof(string)},
+                new DataColumn("RemainingTime", typeof(TimeSpan)) { Caption = "Remaining Time" },
+            },
+            Rows =
+            {
+                {1, "Enhancement", "Extract \"Run JUnit test(s)\" from sub-menu", "Fixed", TimeSpan.Zero},
+                {2, "Enhancement", "Use Eclipse's JDT templating mechanism", "New", new TimeSpan(3, 0, 0, 0)},
+                {3, "Enhancement", "Generate smart templates", "Fixed", TimeSpan.Zero},
+                {4, "Defect", "Implicit default constructor not called", "Accepted", new TimeSpan(1, 0, 0, 0)},
+                {5, "Defect", "Test method body can have duplicate variable names", "Accepted", TimeSpan.Zero},
+                {6, "Defect", "Not all parameter types are supported", "Accepted", TimeSpan.Zero},
+                {7, "Defect", "Variables representing class instance parameters should be initialized with an instance", "Accepted", TimeSpan.Zero},
+                {8, "Defect", "Generating tests for a class which has overloaded methods will produce duplicate test method names", "Accepted", TimeSpan.Zero},
+                {9, "Defect", "Constructor is considered a method to test", "Accepted", TimeSpan.Zero},
+            }
+        };
+
+        public DataTable LoadTasks()
+        {
+            return _tasks;
+        }
+    }
+}
