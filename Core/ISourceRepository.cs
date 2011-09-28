aa2196fb01335ef41a3e1c0d7960c47b37d902f8
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2011-09-28 02:04:34 +0000

    Introducing ISourceRepository and the Mock SourceRepository. Both were modelled after [I]TaskRepository.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@106 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Core/ISourceRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Core
+{
+    /// <summary>
+    /// Represents a system that keeps track of files, folders and their versions.
+    /// </summary>
+    public interface ISourceRepository
+    {
+        /// <summary>
+        /// Loads a <see cref="DataTable"/> representing the available branches.  Pre-filtering is assumed to have
+        /// taken place.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// A <see cref="DataTable"/> with at least the following columns:
+        /// <list type="table">
+        ///     <listheader><term>Column name</term><description>Description</description></listheader>
+        ///     <item><term>ID</term><description>A unique identifier for branches</description></item>
+        ///     <item><term>TaskID</term><description>The ID of the associated task.</description></item>
+        /// </list>
+        /// </returns>
+        DataTable LoadBranches();
+    }
+}
---
+++ Gui/Mock/SourceRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using SoftwareNinjas.BranchAndReviewTools.Core;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Mock
+{
+    internal class SourceRepository : ISourceRepository
+    {
+        private readonly DataTable _branches = new DataTable
+        {
+            Columns =
+            {
+                new DataColumn("ID", typeof(string)) { Caption = "Name" },
+                new DataColumn("TaskID", typeof(int)) { Caption = "Task", ColumnMapping = MappingType.Hidden },
+                new DataColumn("LastActivity", typeof(DateTime)) { Caption = "Last Activity" },
+                {"Owner", typeof(string)},
+                {"Status", typeof(string)},
+            },
+            Rows =
+            {
+                {"123_DoFtp", 123, new DateTime(2011, 09, 02, 14, 32, 06), "Joe Dassin", "unmapped"},
+                {"435_DoSftp", 435, new DateTime(2011, 08, 01, 10, 05, 57), "Tom Jones", "ready for push"},
+                {"436_RefactorInternet", 436, new DateTime(2011, 09, 04, 11, 30, 36), "Paul Anka", "pending changes"},
+            }
+        };
+
+        public DataTable LoadBranches()
+        {
+            return _branches;
+        }
+    }
+}
