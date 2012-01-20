44ad9fac80f5b6e134bc3969feee69c698246682
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-01-20 00:07:37 +0000

    Enhanced the AwesomeGrid to use a TableLayoutPanel.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@291 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

--- Gui/AwesomeGrid.Designer.cs
+++ Gui/AwesomeGrid.Designer.cs
@@ -31,14 +31,18 @@
             this.CaptionLabel = new System.Windows.Forms.Label();
             this.SearchTextBox = new System.Windows.Forms.TextBox();
             this.SearchLabel = new System.Windows.Forms.Label();
+            this.tableLayout = new System.Windows.Forms.TableLayoutPanel();
             this.Grid = new SoftwareNinjas.BranchAndReviewTools.Gui.AccessibleDataGridView();
+            this.tableLayout.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
             this.SuspendLayout();
             // 
             // CaptionLabel
             // 
+            this.CaptionLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.CaptionLabel.AutoSize = true;
             this.CaptionLabel.Location = new System.Drawing.Point(2, 6);
+            this.CaptionLabel.Margin = new System.Windows.Forms.Padding(1);
             this.CaptionLabel.MinimumSize = new System.Drawing.Size(52, 13);
             this.CaptionLabel.Name = "CaptionLabel";
             this.CaptionLabel.Size = new System.Drawing.Size(52, 13);
@@ -46,9 +50,10 @@
             // 
             // SearchTextBox
             // 
-            this.SearchTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.SearchTextBox.Location = new System.Drawing.Point(298, 2);
-            this.SearchTextBox.MinimumSize = new System.Drawing.Size(200, 21);
+            this.SearchTextBox.Margin = new System.Windows.Forms.Padding(1, 2, 1, 1);
+            this.SearchTextBox.MinimumSize = new System.Drawing.Size(20, 21);
             this.SearchTextBox.Name = "SearchTextBox";
             this.SearchTextBox.Size = new System.Drawing.Size(200, 21);
             this.SearchTextBox.TabIndex = 2;
@@ -58,9 +63,10 @@
             // 
             // SearchLabel
             // 
-            this.SearchLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.SearchLabel.AutoSize = true;
             this.SearchLabel.Location = new System.Drawing.Point(240, 6);
+            this.SearchLabel.Margin = new System.Windows.Forms.Padding(1);
             this.SearchLabel.MinimumSize = new System.Drawing.Size(52, 13);
             this.SearchLabel.Name = "SearchLabel";
             this.SearchLabel.Size = new System.Drawing.Size(52, 13);
@@ -68,11 +74,31 @@
             this.SearchLabel.Text = "&Search:";
             this.SearchLabel.Click += new System.EventHandler(this.SearchLabel_Click);
             // 
+            // tableLayout
+            // 
+            this.tableLayout.ColumnCount = 3;
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayout.Controls.Add(this.CaptionLabel, 0, 0);
+            this.tableLayout.Controls.Add(this.Grid, 0, 1);
+            this.tableLayout.Controls.Add(this.SearchTextBox, 2, 0);
+            this.tableLayout.Controls.Add(this.SearchLabel, 1, 0);
+            this.tableLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayout.Location = new System.Drawing.Point(0, 0);
+            this.tableLayout.Name = "tableLayout";
+            this.tableLayout.RowCount = 2;
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayout.Size = new System.Drawing.Size(500, 287);
+            this.tableLayout.TabIndex = 4;
+            // 
             // Grid
             // 
             this.Grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayout.SetColumnSpan(this.Grid, 3);
             this.Grid.Location = new System.Drawing.Point(0, 27);
             this.Grid.Name = "Grid";
             this.Grid.Size = new System.Drawing.Size(500, 260);
@@ -82,15 +108,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.SearchLabel);
-            this.Controls.Add(this.SearchTextBox);
-            this.Controls.Add(this.CaptionLabel);
-            this.Controls.Add(this.Grid);
+            this.Controls.Add(this.tableLayout);
             this.Name = "AwesomeGrid";
             this.Size = new System.Drawing.Size(500, 287);
+            this.tableLayout.ResumeLayout(false);
+            this.tableLayout.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -100,5 +124,6 @@
         public System.Windows.Forms.Label CaptionLabel;
         public System.Windows.Forms.TextBox SearchTextBox;
         public System.Windows.Forms.Label SearchLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayout;
     }
 }
