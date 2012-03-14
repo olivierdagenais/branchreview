b8d5027308e745e3e202a5b87786185d9802d715
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-03-14 14:18:24 +0000

    Introducing the UnsupportedList, to facilitate some upcoming work.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@302 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui/UnsupportedList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui
+{
+    public abstract class UnsupportedList<T> : IList<T>
+    {
+        public virtual IEnumerator<T> GetEnumerator()
+        {
+            throw new NotSupportedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public virtual void Add(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual void Clear()
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual bool Contains(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual bool Remove(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual int Count
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        public virtual bool IsReadOnly
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        public virtual int IndexOf(T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual void Insert(int index, T item)
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual void RemoveAt(int index)
+        {
+            throw new NotSupportedException();
+        }
+
+        public virtual T this[int index]
+        {
+            get { throw new NotSupportedException(); }
+            set { throw new NotSupportedException(); }
+        }
+    }
+}
