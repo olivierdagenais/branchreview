a1039865ab658ac81cc39fc3296e17a1ab32dce6
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-03-23 13:16:38 +0000

    Introducing a registry-based current user settings persistence mechanism.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@325 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui/Extensions/ControlExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using SoftwareNinjas.Core;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Extensions
+{
+    internal static class ControlExtensions
+    {
+        private static readonly string ApplicationHome = "Software".CombinePath(
+            Assembly.GetCallingAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company,
+            Assembly.GetCallingAssembly().GetCustomAttribute<AssemblyProductAttribute>().Product
+        );
+
+        public static T LoadSetting<T>(this Control control, Expression<Func<T>> propertyExpression, T defaultValue)
+        {
+            using (var sk = Registry.CurrentUser.CreateSubKey(ApplicationHome))
+            {
+                Debug.Assert(sk != null);
+
+                var name = propertyExpression.DetermineName();
+                var settingName = control.Name + name;
+                var value = (T) sk.GetValue(settingName, defaultValue);
+                return value;
+            }
+        }
+
+        public static void SaveSetting<T>(this Control control, Expression<Func<T>> propertyExpression)
+        {
+            using (var sk = Registry.CurrentUser.CreateSubKey(ApplicationHome))
+            {
+                Debug.Assert(sk != null);
+
+                var name = propertyExpression.DetermineName();
+                var settingName = control.Name + name;
+                var func = propertyExpression.Compile();
+                var value = func();
+                sk.SetValue(settingName, value);
+            }
+        }
+
+    }
+}
---
+++ Gui/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Extensions
+{
+    public static class ExpressionExtensions
+    {
+        /// <remarks>
+        /// Derived from <see href="https://gist.github.com/1377167">Rename-proof argument validation in C#</see>.
+        /// </remarks>
+        public static string DetermineName<T>(this Expression<Func<T>> valueExpression)
+        {
+            var expression = valueExpression.Body;
+            if (expression.NodeType == ExpressionType.Convert)
+            {
+                // "A cast or conversion operation"
+                var unaryExpression = (UnaryExpression) expression;
+                expression = unaryExpression.Operand;
+            }
+            switch (expression.NodeType)
+            {
+                case ExpressionType.MemberAccess:
+                    var memberExpression = (MemberExpression) expression;
+                    var name = memberExpression.Member.Name;
+                    return name;
+                default:
+                    throw new NotImplementedException(
+                        "Invalid use of valueExpression:  Only member access is supported.  "
+                        + "Use an expression that looks like '() => member'");
+            }
+        }
+    }
+}
