0da016e127677c53231677a55c8fb59840b38c00
olivier.dagenais <olivier.dagenais@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-03-23 13:16:01 +0000

    Imported the ControlCollectionExtensions.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@320 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui/Extensions/ControlCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Forms;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Extensions
+{
+    public static class ControlCollectionExtensions
+    {
+        public static Control RemoveLast(this Control.ControlCollection collection)
+        {
+            Control result = null;
+            var lastItemIndex = collection.Count - 1;
+            if (lastItemIndex >= 0)
+            {
+                result = collection[lastItemIndex];
+                collection.RemoveAt(lastItemIndex);
+            }
+            return result;
+        }
+
+        public static Control GetLast(this Control.ControlCollection collection)
+        {
+            Control result = null;
+            var lastItemIndex = collection.Count - 1;
+            if (lastItemIndex >= 0)
+            {
+                result = collection[lastItemIndex];
+            }
+            return result;
+        }
+    }
+}
