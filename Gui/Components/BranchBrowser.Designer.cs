9a7ce24dee3b924bbb65a7832d655dc6e8f4d705
olivier.dagenais@gmail.com <olivier.dagenais@gmail.com@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-06-11 18:10:11 +0000

    Introducing the BranchBrowser component.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@359 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui/Components/BranchBrowser.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SoftwareNinjas.BranchAndReviewTools.Gui.Components
+{
+    partial class BranchBrowser
+    {
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.branchGrid = new SoftwareNinjas.BranchAndReviewTools.Gui.Grids.AwesomeGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.branchGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // branchGrid
+            // 
+            this.branchGrid.Caption = "";
+            this.branchGrid.DataTable = null;
+            this.branchGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.branchGrid.Filter = null;
+            this.branchGrid.Location = new System.Drawing.Point(0, 0);
+            this.branchGrid.Name = "branchGrid";
+            this.branchGrid.Size = new System.Drawing.Size(776, 87);
+            this.branchGrid.TabIndex = 0;
+            this.branchGrid.Title = "Branches";
+            this.branchGrid.ContextMenuStripNeeded += new SoftwareNinjas.BranchAndReviewTools.Gui.Grids.ContextMenuNeededEventHandler(this.branchGrid_ContextMenuNeeded);
+            this.branchGrid.RowInvoked += new System.EventHandler(this.branchGrid_RowInvoked);
+            // 
+            // BranchBrowser
+            // 
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.branchGrid);
+            this.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Name = "BranchBrowser";
+            ((System.ComponentModel.ISupportInitialize)(this.branchGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SoftwareNinjas.BranchAndReviewTools.Gui.Grids.AwesomeGrid branchGrid;
+    }
+}
---
+++ Gui/Components/BranchBrowser.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Windows.Forms;
+using SoftwareNinjas.BranchAndReviewTools.Core;
+using SoftwareNinjas.BranchAndReviewTools.Gui.Extensions;
+using SoftwareNinjas.BranchAndReviewTools.Gui.Grids;
+using SoftwareNinjas.BranchAndReviewTools.Gui.History;
+using SoftwareNinjas.Core;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Components
+{
+    public partial class BranchBrowser : Control, IHistoryItem
+    {
+        private readonly ITaskRepository _taskRepository;
+        private readonly ISourceRepository _sourceRepository;
+        private readonly IShelvesetRepository _shelvesetRepository;
+
+        public BranchBrowser
+        (ITaskRepository taskRepository, ISourceRepository sourceRepository, IShelvesetRepository shelvesetRepository)
+        {
+            _taskRepository = taskRepository;
+            _sourceRepository = sourceRepository;
+            _shelvesetRepository = shelvesetRepository;
+
+            InitializeComponent();
+            branchGrid.Grid.MultiSelect = false;
+            this.ExecuteLater(10, () => SwitchCurrentTab(true));
+        }
+
+        #region Common
+
+        private void SetCurrentBranch(object branchId, object taskId)
+        {
+            this.ToDo("Create an ActivityBrowser, initialize it for branch {0} and push it", branchId);
+        }
+
+        private void StartWorkOnBranch(object branchId, object taskId)
+        {
+            this.ToDo("Create a ChangeInspector, initialize it for branch {0} in commit mode and push it", branchId);
+        }
+
+        private void SwitchCurrentTab(bool refresh)
+        {
+            if (branchGrid.DataTable == null || refresh)
+            {
+                branchGrid.DataTable = _sourceRepository.LoadBranches();
+                var branchCount = branchGrid.DataTable.Rows.Count;
+                branchGrid.Caption = "{0} branch{1}".FormatInvariant(branchCount, branchCount == 1 ? "" : "es");
+            }												
+
+            this.ExecuteLater(10, () => branchGrid.Focus());
+        }
+
+        #endregion
+
+        #region Branches
+
+        private void AddBranchSpecificActions(Menu.MenuItemCollection items, bool needsLeadingSeparator)
+        {
+            var selectedRows = branchGrid.Grid.SelectedRows;
+            if (selectedRows.Count > 0)
+            {
+                if (needsLeadingSeparator)
+                {
+                    items.AddSeparator();
+                }
+                var row = selectedRows[0].DataRow;
+                var branchId = row["ID"];
+                var taskId = row["TaskID"];
+                var builtInActions = new[]
+                {
+                    new MenuAction("defaultInspect", "&Inspect", true,
+                                () => SetCurrentBranch(branchId, taskId) ),
+                    new MenuAction("defaultOpen", "&Work on this", row["BasePath"] != DBNull.Value,
+                                () => StartWorkOnBranch(branchId, taskId) ),
+                };
+                items.AddActions(builtInActions);
+                var specificActions = _sourceRepository.GetBranchActions(branchId);
+                if (specificActions.Count > 0)
+                {
+                    items.AddSeparator();
+                    items.AddActions(specificActions);
+                }
+            }
+        }
+
+        private void branchGrid_ContextMenuNeeded(object sender, ContextMenuNeededEventArgs e)
+        {
+            var menu = BuildBranchActionMenu();
+            e.ContextMenu = menu;
+        }
+
+        private void branchGrid_RowInvoked(object sender, EventArgs e)
+        {
+            InvokeDefaultBranchGridAction();
+        }
+
+        private ContextMenu BuildBranchActionMenu()
+        {
+            var menu = new ContextMenu();
+            AddBranchSpecificActions(menu.MenuItems, false);
+            return menu;
+        }
+
+        private void InvokeDefaultBranchGridAction()
+        {
+            var menu = BuildBranchActionMenu();
+            menu.MenuItems.InvokeFirstMenuItem();
+        }
+
+        #endregion
+
+        #region IHistoryItem Members
+
+        IHistoryContainer IHistoryItem.Container { get; set; }
+
+        public Control Control { get { return this; } }
+
+        public string Title { get { return "Branches"; } }
+
+        public void Reload()
+        {
+            SwitchCurrentTab(true);
+        }
+        #endregion
+    }
+}
