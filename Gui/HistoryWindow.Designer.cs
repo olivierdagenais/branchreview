e9099ebbd14f23a73c8bcb14ea3ab583432ad281
olivier.dagenais@gmail.com <olivier.dagenais@gmail.com@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-06-11 18:09:00 +0000

    Introducing the HistoryWindow.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@353 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui/HistoryWindow.Designer.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Forms;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui
+{
+    partial class HistoryWindow
+    {
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.historyContainer = new SoftwareNinjas.BranchAndReviewTools.Gui.History.HistoryContainer();
+            this.SuspendLayout();
+            //
+            // historyContainer
+            //
+            this.historyContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.historyContainer.Location = new System.Drawing.Point(0, 0);
+            this.historyContainer.Name = "historyContainer";
+            this.historyContainer.Size = new System.Drawing.Size(792, 550);
+            this.historyContainer.TabIndex = 0;
+            // 
+            // HistoryWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.historyContainer);
+            this.KeyPreview = true;
+            this.Name = "HistoryWindow";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SoftwareNinjas.BranchAndReviewTools.Gui.History.HistoryContainer historyContainer;
+    }
+}
---
+++ Gui/HistoryWindow.cs
@@ -0,0 +1,20 @@
+﻿using SoftwareNinjas.BranchAndReviewTools.Gui.History;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui
+{
+    public partial class HistoryWindow : DockContent
+    {
+        public HistoryWindow(IHistoryItem historyItem)
+        {
+            InitializeComponent();
+            historyContainer.Navigated += historyContainer_Navigated;
+            historyContainer.Push(historyItem);
+        }
+
+        void historyContainer_Navigated(object sender, System.EventArgs e)
+        {
+            this.Text = historyContainer.Current.Title;
+        }
+    }
+}
