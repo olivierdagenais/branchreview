1e780f504dd18399186bd5ea849079e537be5c23
olivier.dagenais@gmail.com <olivier.dagenais@gmail.com@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-06-11 18:10:20 +0000

    Moved ObjectExtensions to Core.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@360 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Core/ObjectExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Core
+{
+    /// <summary>
+    /// Extensions for instances of type <see cref="Object"/>.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// A description of work still to be done, which will be emitted to the Debug Listeners.
+        /// </summary>
+        /// 
+        /// <param name="o">
+        /// Unimportant parameter, used for the extension method.
+        /// </param>
+        /// 
+        /// <param name="message">
+        /// The contents of the TODO itself, optionally formatted with arguments.
+        /// </param>
+        /// 
+        /// <param name="args">
+        /// An object array containing zero or more objects to format.
+        /// </param>
+        public static void ToDo(this object o, string message, params object[] args)
+        {
+            var line = String.Format(message, args);
+            Debug.WriteLine(line, "TODO");
+        }
+    }
+}
