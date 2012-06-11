848bf7211e73d0a47ddb58ac992c902da8de086a
olivier.dagenais@gmail.com <olivier.dagenais@gmail.com@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-06-11 18:12:35 +0000

    Introducing DockPanelExtensions, adapted from a SourceForge.net forum post.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@370 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui/Extensions/DockPanelExtensions.cs
@@ -0,0 +1,65 @@
+﻿using SoftwareNinjas.BranchAndReviewTools.Gui.WeifenLuo.WinFormsUI.Docking;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Extensions
+{
+    /// <remarks>
+    /// Adapted from <see href="http://sourceforge.net/projects/dockpanelsuite/forums/forum/402316/topic/1526067"/>.
+    /// </remarks>
+    public static class DockPanelExtensions
+    {
+        public static IDockContent GetFirstDocument(this DockPanel dockPanel)
+        {
+            if (dockPanel.ActivePane == null)
+                return null;
+            if (dockPanel.ActivePane.Contents.Count == 0)
+                return null;
+
+            return dockPanel.ActivePane.Contents[0];
+        }
+
+        // Returns the last DockContent in ActivePane:
+        public static IDockContent GetLastDocument(this DockPanel dockPanel)
+        {
+            if (dockPanel.ActivePane == null)
+                return null;
+            if (dockPanel.ActivePane.Contents.Count == 0)
+                return null;
+
+            return dockPanel.ActivePane.Contents[dockPanel.ActivePane.Contents.Count - 1];
+        }
+
+        // Returns the previous DockContent in ActivePane, prior to the ActiveContent:
+        public static IDockContent GetPreviousDocument(this DockPanel dockPanel)
+        {
+            if (dockPanel.ActivePane == null)
+                return null;
+            bool getPreviousDocument = false;
+            for (int x = dockPanel.ActivePane.Contents.Count - 1; x > -1; x--)
+            {
+                DockContent dockContent = (DockContent)dockPanel.ActivePane.Contents[x];
+                if (dockContent == dockPanel.ActivePane.ActiveContent)
+                    getPreviousDocument = true;
+                else if (getPreviousDocument && !dockContent.IsHidden)
+                    return dockContent;
+            }
+            return null;
+        }
+
+        // Returns the next DockContent in ActivePane, following the ActiveContent:
+        public static IDockContent GetNextDocument(this DockPanel dockPanel)
+        {
+            if (dockPanel.ActivePane == null)
+                return null;
+            bool getNextDocument = false;
+            for (int x = 0; x < dockPanel.ActivePane.Contents.Count; x++)
+            {
+                DockContent dockContent = (DockContent)dockPanel.ActivePane.Contents[x];
+                if (dockContent == dockPanel.ActivePane.ActiveContent)
+                    getNextDocument = true;
+                else if (getNextDocument && !dockContent.IsHidden)
+                    return dockContent;
+            }
+            return null;
+        }
+    }
+}
