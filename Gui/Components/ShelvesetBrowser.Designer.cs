9a017d59389b22175f227bf66c136a533a23323e
olivier.dagenais@gmail.com <olivier.dagenais@gmail.com@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-06-11 18:14:24 +0000

    Introducing the ShelvesetBrowser component.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@394 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui/Components/ShelvesetBrowser.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SoftwareNinjas.BranchAndReviewTools.Gui.Components
+{
+    partial class ShelvesetBrowser
+    {
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shelvesetGrid = new SoftwareNinjas.BranchAndReviewTools.Gui.Grids.AwesomeGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.shelvesetGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // shelvesetGrid
+            // 
+            this.shelvesetGrid.Caption = "";
+            this.shelvesetGrid.DataTable = null;
+            this.shelvesetGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.shelvesetGrid.Filter = null;
+            this.shelvesetGrid.Location = new System.Drawing.Point(0, 0);
+            this.shelvesetGrid.Name = "shelvesetGrid";
+            this.shelvesetGrid.Size = new System.Drawing.Size(776, 83);
+            this.shelvesetGrid.TabIndex = 0;
+            this.shelvesetGrid.Title = "Shelvesets";
+            this.shelvesetGrid.ContextMenuStripNeeded += new SoftwareNinjas.BranchAndReviewTools.Gui.Grids.ContextMenuNeededEventHandler(this.shelvesetGrid_ContextMenuNeeded);
+            this.shelvesetGrid.RowInvoked += new System.EventHandler(this.shelvesetGrid_RowInvoked);
+            // 
+            // ShelvesetBrowser
+            // 
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.shelvesetGrid);
+            this.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Name = "ShelvesetBrowser";
+            ((System.ComponentModel.ISupportInitialize)(this.shelvesetGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SoftwareNinjas.BranchAndReviewTools.Gui.Grids.AwesomeGrid shelvesetGrid;
+    }
+}
---
+++ Gui/Components/ShelvesetBrowser.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Windows.Forms;
+using SoftwareNinjas.BranchAndReviewTools.Core;
+using SoftwareNinjas.BranchAndReviewTools.Gui.Extensions;
+using SoftwareNinjas.BranchAndReviewTools.Gui.Grids;
+using SoftwareNinjas.BranchAndReviewTools.Gui.History;
+using SoftwareNinjas.Core;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Components
+{
+    public partial class ShelvesetBrowser : Control, IHistoryItem
+    {
+        private readonly ITaskRepository _taskRepository;
+        private readonly ISourceRepository _sourceRepository;
+        private readonly IShelvesetRepository _shelvesetRepository;
+
+        public ShelvesetBrowser
+        (ITaskRepository taskRepository, ISourceRepository sourceRepository, IShelvesetRepository shelvesetRepository)
+        {
+            _taskRepository = taskRepository;
+            _sourceRepository = sourceRepository;
+            _shelvesetRepository = shelvesetRepository;
+
+            InitializeComponent();
+            shelvesetGrid.Grid.MultiSelect = false;
+            this.ExecuteLater(10, () => SwitchCurrentTab(true));
+        }
+
+        #region Common
+
+        private void SetCurrentShelveset(object shelvesetId, string shelvesetName)
+        {
+            this.ToDo("Create a ShelvesetInspector, initialize it for shelveset {0} and push it", shelvesetId);
+        }
+
+        private void SwitchCurrentTab(bool refresh)
+        {
+            if (shelvesetGrid.DataTable == null || refresh)
+            {
+                shelvesetGrid.DataTable = _shelvesetRepository.LoadShelvesets();
+                var shelvesetCount = shelvesetGrid.DataTable.Rows.Count;
+                shelvesetGrid.Caption = "{0} shelveset{1}".FormatInvariant(shelvesetCount, shelvesetCount == 1 ? "" : "s");
+            }
+
+            this.ExecuteLater(10, () => shelvesetGrid.Focus());
+        }
+
+        #endregion
+
+        #region Shelvesets
+
+        private void AddShelvesetSpecificActions(Menu.MenuItemCollection items, bool needsLeadingSeparator)
+        {
+            var selectedItems = shelvesetGrid.Grid.SelectedRows;
+            if (selectedItems.Count > 0)
+            {
+                if (needsLeadingSeparator)
+                {
+                    items.AddSeparator();
+                }
+                var row = selectedItems[0].DataRow;
+                var shelvesetId = row["ID"];
+                var shelvesetName = (string) row["Name"];
+                var builtInActions = new[]
+                {
+                    new MenuAction("defaultInspect", "&Inspect", true,
+                                   () => SetCurrentShelveset(shelvesetId, shelvesetName) ),
+                };
+                items.AddActions(builtInActions);
+                var specificActions = _shelvesetRepository.GetShelvesetActions(shelvesetId);
+                if (specificActions.Count > 0)
+                {
+                    items.AddSeparator();
+                    items.AddActions(specificActions);
+                }
+            }
+        }
+
+        private void shelvesetGrid_ContextMenuNeeded(object sender, ContextMenuNeededEventArgs e)
+        {
+            var menu = BuildShelvesetActionMenu();
+            e.ContextMenu = menu;
+        }
+
+        private void shelvesetGrid_RowInvoked(object sender, EventArgs e)
+        {
+            InvokeDefaultShelvesetGridAction();
+        }
+
+        private ContextMenu BuildShelvesetActionMenu()
+        {
+            var menu = new ContextMenu();
+            AddShelvesetSpecificActions(menu.MenuItems, false);
+            return menu;
+        }
+
+        private void InvokeDefaultShelvesetGridAction()
+        {
+            var menu = BuildShelvesetActionMenu();
+            menu.MenuItems.InvokeFirstMenuItem();
+        }
+
+        #endregion
+
+        #region IHistoryItem Members
+
+        IHistoryContainer IHistoryItem.Container { get; set; }
+
+        public Control Control { get { return this; } }
+
+        public string Title { get { return "Shelvesets"; } }
+
+        public void Reload()
+        {
+            SwitchCurrentTab(true);
+        }
+        #endregion
+    }
+}
