9eb41637e6d94f202b3c79aa135262fbcbf6046e
olivier.dagenais@gmail.com <olivier.dagenais@gmail.com@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-06-11 18:09:09 +0000

    Introducing TabbedMain for hosting IHistoryContainer implementations such as HistoryWindow.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@354 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui/TabbedMain.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SoftwareNinjas.BranchAndReviewTools.Gui
+{
+    partial class TabbedMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && ( components != null ))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.statusBarText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusBarProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.mainPanel = new WeifenLuo.WinFormsUI.Docking.DockPanel();
+            this.menuStrip = new System.Windows.Forms.MainMenu();
+            this.fileMenu = new System.Windows.Forms.MenuItem();
+            this.exitMenuItem = new System.Windows.Forms.MenuItem();
+            this.editMenu = new System.Windows.Forms.MenuItem();
+            this.searchMenuItem = new System.Windows.Forms.MenuItem();
+            this.viewMenu = new System.Windows.Forms.MenuItem();
+            this.refreshMenuItem = new System.Windows.Forms.MenuItem();
+            this.statusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusBar
+            // 
+            this.statusBar.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBarText,
+            this.statusBarProgress});
+            this.statusBar.Location = new System.Drawing.Point(0, 551);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(792, 22);
+            this.statusBar.TabIndex = 0;
+            // 
+            // statusBarText
+            // 
+            this.statusBarText.Image = global::SoftwareNinjas.BranchAndReviewTools.Gui.Properties.Resources.dialog_information;
+            this.statusBarText.Name = "statusBarText";
+            this.statusBarText.Size = new System.Drawing.Size(59, 17);
+            this.statusBarText.Text = "Ready";
+            // 
+            // statusBarProgress
+            // 
+            this.statusBarProgress.Name = "statusBarProgress";
+            this.statusBarProgress.Size = new System.Drawing.Size(100, 16);
+            //
+            // mainPanel
+            //
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.DockBackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(792, 573);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.fileMenu,
+            this.editMenu,
+            this.viewMenu});
+            this.menuStrip.Name = "menuStrip";
+            // 
+            // fileMenu
+            // 
+            this.fileMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {                                
+            this.exitMenuItem});
+            this.fileMenu.Name = "fileMenu";
+            this.fileMenu.Text = "&File";
+            // 
+            // exitMenuItem
+            // 
+            this.exitMenuItem.Name = "exitMenuItem";
+            this.exitMenuItem.Text = "E&xit";
+            this.exitMenuItem.Click += new System.EventHandler(this.exitMenuItem_Click);
+            // 
+            // editMenu
+            // 
+            this.editMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.searchMenuItem});
+            this.editMenu.Name = "editMenu";
+            this.editMenu.Text = "&Edit";
+            // 
+            // searchMenuItem
+            // 
+            this.searchMenuItem.Name = "searchMenuItem";
+            this.searchMenuItem.ShowShortcut = true;
+            this.searchMenuItem.Text = "&Search\tCtrl+F";
+            this.searchMenuItem.Click += new System.EventHandler(this.searchMenuItem_Click);
+            // 
+            // viewMenu
+            // 
+            this.viewMenu.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.refreshMenuItem});
+            this.viewMenu.Name = "viewMenu";
+            this.viewMenu.Text = "&View";
+            // 
+            // refreshMenuItem
+            // 
+            this.refreshMenuItem.Name = "refreshMenuItem";
+            this.refreshMenuItem.Shortcut = System.Windows.Forms.Shortcut.F5;
+            this.refreshMenuItem.Text = "&Refresh";
+            this.refreshMenuItem.Click += new System.EventHandler(this.refreshMenuItem_Click);
+            // 
+            // TabbedMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.statusBar);
+            this.KeyPreview = true;
+            this.Menu = this.menuStrip;
+            this.Name = "TabbedMain";
+            this.Text = "Branch and Review Tools";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Main_KeyUp);
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel statusBarText;
+        private System.Windows.Forms.ToolStripProgressBar statusBarProgress;
+        private WeifenLuo.WinFormsUI.Docking.DockPanel mainPanel;
+        private System.Windows.Forms.MainMenu menuStrip;
+        private System.Windows.Forms.MenuItem fileMenu;
+        private System.Windows.Forms.MenuItem exitMenuItem;
+        private System.Windows.Forms.MenuItem viewMenu;
+        private System.Windows.Forms.MenuItem refreshMenuItem;
+        private System.Windows.Forms.MenuItem editMenu;
+        private System.Windows.Forms.MenuItem searchMenuItem;
+    }
+}
---
+++ Gui/TabbedMain.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition.Hosting;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using ScintillaNet;
+using SoftwareNinjas.BranchAndReviewTools.Core;
+using SoftwareNinjas.BranchAndReviewTools.Gui.Extensions;
+using SoftwareNinjas.BranchAndReviewTools.Gui.Grids;
+using SoftwareNinjas.BranchAndReviewTools.Gui.History;
+using SoftwareNinjas.BranchAndReviewTools.Gui.Properties;
+using SoftwareNinjas.Core;
+using EnumExtensions = SoftwareNinjas.BranchAndReviewTools.Gui.Extensions.EnumExtensions;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui
+{
+    public partial class TabbedMain : Form, ILog
+    {
+        private const int StatusMessageCapacity = 512;
+        private readonly ITaskRepository _taskRepository;
+        private readonly ISourceRepository _sourceRepository;
+        private readonly IShelvesetRepository _shelvesetRepository;
+        private readonly LinkedList<StatusMessage> _statusMessages = new LinkedList<StatusMessage>();
+        private readonly Throttler _statusThrottle;
+
+        public TabbedMain()
+        {
+            InitializeComponent();
+            Load += Main_Load;
+            FormClosing += Main_Closing;
+            _statusThrottle = new Throttler(100, UpdateStatusBar);
+            #if DEBUG
+            _taskRepository = new Core.Mock.TaskRepository();
+            _sourceRepository = new Core.Mock.SourceRepository();
+            _shelvesetRepository = new Core.Mock.ShelvesetRepository();
+            #else
+            var catalog = new DirectoryCatalog("Repositories");
+            var container = new CompositionContainer(catalog);
+            _taskRepository = container.GetExportedValueOrDefault<ITaskRepository>();
+            _sourceRepository = container.GetExportedValueOrDefault<ISourceRepository>();
+            _shelvesetRepository = container.GetExportedValueOrDefault<IShelvesetRepository>();
+            #endif
+
+            // ReSharper disable HeuristicUnreachableCode
+            // ReSharper disable ConditionIsAlwaysTrueOrFalse
+            if (_sourceRepository != null)
+            // ReSharper restore ConditionIsAlwaysTrueOrFalse
+            {
+                _sourceRepository.Log = this;
+            }
+
+            // ReSharper disable ConditionIsAlwaysTrueOrFalse
+            if (_taskRepository != null)
+            // ReSharper restore ConditionIsAlwaysTrueOrFalse
+            {
+                _taskRepository.Log = this;
+            }
+
+            // ReSharper disable ConditionIsAlwaysTrueOrFalse
+            if (_shelvesetRepository != null)
+            // ReSharper restore ConditionIsAlwaysTrueOrFalse
+            {
+                _shelvesetRepository.Log = this;
+            }
+
+            // ReSharper restore HeuristicUnreachableCode
+        }
+
+        void Main_Load(object sender, EventArgs e)
+        {
+            Size = this.LoadSetting(() => Size, new Size(800, 600), SizeExtensions.Parse);
+            Location = this.LoadSetting(() => Location, Point.Empty, PointExtensions.Parse);
+            WindowState = 
+                this.LoadSetting(() => WindowState, FormWindowState.Normal, EnumExtensions.Parse<FormWindowState>);
+
+            Start(Environment.GetCommandLineArgs());
+        }
+
+        void Main_Closing(object sender, CancelEventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.Location = RestoreBounds.Location;
+                this.Size = RestoreBounds.Size;
+            }
+            this.SaveSetting(() => WindowState);
+            this.SaveSetting(() => Location);
+            this.SaveSetting(() => Size);
+        }
+
+        void Main_KeyUp(object sender, KeyEventArgs keyEventArgs)
+        {
+            Func<IHistoryContainer, bool> canMove = null;
+            Action<IHistoryContainer> move = null;
+            var keyData = keyEventArgs.KeyData;
+            if (keyData == Keys.BrowserBack || keyData == (Keys.Alt | Keys.Left))
+            {
+                canMove = ihc => ihc.CanGoBack;
+                move = ihc => ihc.GoBack();
+            }
+            else if (keyData == Keys.BrowserForward || keyData == (Keys.Alt | Keys.Right))
+            {
+                canMove = ihc => ihc.CanGoForward;
+                move = ihc => ihc.GoForward();
+            }
+
+            if (canMove != null && move != null)
+            {
+                IHistoryContainer container = null;
+
+                if (container != null)
+                {
+                    if (canMove(container))
+                    {
+                        move(container);
+                    }
+                }
+                keyEventArgs.Handled = true;
+            }
+        }
+
+        public void Start(IEnumerable<string> arguments)
+        {
+            // TODO: parse arguments and do something with them.
+        }
+
+        #region Menu items
+
+        private void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void refreshMenuItem_Click(object sender, EventArgs e)
+        {
+            // TODO: push refresh event to current tab
+        }
+
+        #endregion
+
+        #region Common
+
+        private void searchMenuItem_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send(/* Ctrl+F */ "^f");
+        }
+
+        #endregion
+
+        #region Implementation of ILog
+
+        public void Info(string message)
+        {
+            Enqueue(StatusKind.Info, message);
+        }
+
+        public void Info(string message, int progressValue, int progressMaximum)
+        {
+            Enqueue(StatusKind.Info, message, progressValue, progressMaximum);
+        }
+
+        public void Warning(string message)
+        {
+            Enqueue(StatusKind.Warning, message);
+        }
+
+        public void Error(string message)
+        {
+            Enqueue(StatusKind.Error, message);
+        }
+
+        private void Enqueue(StatusKind statusKind, string message)
+        {
+            Enqueue(new StatusMessage(statusKind, message));
+        }
+
+        private void Enqueue(StatusKind statusKind, string message, int progressValue, int progressMaximumValue)
+        {
+            Enqueue(new StatusMessage(statusKind, message, progressValue, progressMaximumValue));
+        }
+
+        private void Enqueue(StatusMessage statusMessage)
+        {
+            lock (_statusMessages)
+            {
+                _statusMessages.AddLast(statusMessage);
+                if (_statusMessages.Count > StatusMessageCapacity)
+                {
+                    _statusMessages.RemoveFirst();
+                }
+            }
+            _statusThrottle.Fire();
+        }
+
+        // TODO: light-up on Windows 7 by displaying progress in the task bar and changing the back colour
+        // depending on the log type
+        private void UpdateStatusBar()
+        {
+            StatusMessage last;
+            lock (_statusMessages)
+            {
+                last = _statusMessages.Last.Value;
+            }
+            statusBarText.Text = last.Message;
+            switch (last.StatusKind)
+            {
+                case StatusKind.Info:
+                    statusBarText.Image = Resources.dialog_information;
+                    if (last.ProgressValue.HasValue && last.ProgressMaximumValue.HasValue)
+                    {
+                        if (0 == last.ProgressMaximumValue.Value)
+                        {
+                            statusBarProgress.Style = ProgressBarStyle.Marquee;
+                        }
+                        else
+                        {
+                            statusBarProgress.Style = ProgressBarStyle.Continuous;
+                            statusBarProgress.Maximum = last.ProgressMaximumValue.Value;
+                            statusBarProgress.Value = last.ProgressValue.Value;
+                        }
+                    }
+                    break;
+                case StatusKind.Warning:
+                    statusBarText.Image = Resources.dialog_warning;
+                    statusBarProgress.Value = 0;
+                    break;
+                case StatusKind.Error:
+                    statusBarText.Image = Resources.dialog_error;
+                    statusBarProgress.Value = 0;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        #endregion
+    }
+}
