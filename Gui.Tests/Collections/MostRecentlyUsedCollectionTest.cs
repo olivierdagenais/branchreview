57489d33473de2f8d2cf8eb7f4cc80ef010962a5
olivier.dagenais@gmail.com <olivier.dagenais@gmail.com@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-06-11 18:12:51 +0000

    Introducing MostRecentlyUsedCollection[Test] skeletons.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@373 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui.Tests/Collections/MostRecentlyUsedCollectionTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using SoftwareNinjas.BranchAndReviewTools.Gui.Collections;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Tests.Collections
+{
+    /// <summary>
+    /// A class to test <see cref="MostRecentlyUsedCollection{T}"/>.
+    /// </summary>
+    [TestFixture]
+    public class MostRecentlyUsedCollectionTest
+    {
+    }
+}
---
+++ Gui/Collections/MostRecentlyUsedCollection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Collections
+{
+    public class MostRecentlyUsedCollection<T> : ICollection<T>
+    {
+        #region Implementation of IEnumerable
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Implementation of ICollection<T>
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+    }
+}
