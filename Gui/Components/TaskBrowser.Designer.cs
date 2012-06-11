d7245ba4c2a8ea31f20fc7b0c5ba0d59fa1c1692
olivier.dagenais@gmail.com <olivier.dagenais@gmail.com@4154e2d8-f3f9-11dd-87c9-111d11f6bf44>
2012-06-11 18:09:55 +0000

    Introducing the TaskBrowser component.
    git-svn-id: http://branchreview.googlecode.com/svn/trunk@357 4154e2d8-f3f9-11dd-87c9-111d11f6bf44

---
+++ Gui/Components/TaskBrowser.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SoftwareNinjas.BranchAndReviewTools.Gui.Components
+{
+    partial class TaskBrowser
+    {
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.taskGrid = new SoftwareNinjas.BranchAndReviewTools.Gui.Grids.AwesomeGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.taskGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // taskGrid
+            // 
+            this.taskGrid.Caption = "";
+            this.taskGrid.DataTable = null;
+            this.taskGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.taskGrid.Filter = null;
+            this.taskGrid.Location = new System.Drawing.Point(3, 3);
+            this.taskGrid.Name = "taskGrid";
+            this.taskGrid.Size = new System.Drawing.Size(778, 495);
+            this.taskGrid.TabIndex = 0;
+            this.taskGrid.Title = "Tasks";
+            this.taskGrid.ContextMenuStripNeeded += new SoftwareNinjas.BranchAndReviewTools.Gui.Grids.ContextMenuNeededEventHandler(this.taskGrid_ContextMenuNeeded);
+            this.taskGrid.RowInvoked += new System.EventHandler(this.taskGrid_RowInvoked);
+            // 
+            // TaskBrowser
+            // 
+            this.ClientSize = new System.Drawing.Size(792, 573);
+            this.Controls.Add(this.taskGrid);
+            this.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Name = "TaskBrowser";
+            ((System.ComponentModel.ISupportInitialize)(this.taskGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SoftwareNinjas.BranchAndReviewTools.Gui.Grids.AwesomeGrid taskGrid;
+    }
+}
---
+++ Gui/Components/TaskBrowser.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using SoftwareNinjas.BranchAndReviewTools.Core;
+using SoftwareNinjas.BranchAndReviewTools.Gui.Extensions;
+using SoftwareNinjas.BranchAndReviewTools.Gui.Grids;
+using SoftwareNinjas.BranchAndReviewTools.Gui.History;
+using SoftwareNinjas.Core;
+
+namespace SoftwareNinjas.BranchAndReviewTools.Gui.Components
+{
+    public partial class TaskBrowser : Control, IHistoryItem
+    {
+        private readonly ITaskRepository _taskRepository;
+        private readonly ISourceRepository _sourceRepository;
+        private readonly IShelvesetRepository _shelvesetRepository;
+
+        public TaskBrowser
+        (ITaskRepository taskRepository, ISourceRepository sourceRepository, IShelvesetRepository shelvesetRepository)
+        {
+            _taskRepository = taskRepository;
+            _sourceRepository = sourceRepository;
+            _shelvesetRepository = shelvesetRepository;
+
+            InitializeComponent();
+            taskGrid.Grid.MultiSelect = false;
+            this.ExecuteLater(10, () => SwitchCurrentTab(true));
+        }
+
+        #region Common
+
+        private void SwitchCurrentTab(bool refresh)
+        {
+            if (taskGrid.DataTable == null || refresh)
+            {
+                taskGrid.DataTable = _taskRepository.LoadTasks();
+                var taskCount = taskGrid.DataTable.Rows.Count;
+                taskGrid.Caption = "{0} task{1}".FormatInvariant(taskCount, taskCount == 1 ? "" : "s");
+            }
+
+            this.ExecuteLater(10, () => taskGrid.Focus());
+        }
+
+        #endregion
+
+        #region Tasks
+
+        private void AddTaskSpecificActions(Menu.MenuItemCollection items, bool needsLeadingSeparator)
+        {
+            var taskId = taskGrid.FindSelectedId();
+            if (taskId != null)
+            {
+                var specificActions = _taskRepository.GetTaskActions(taskId);
+                if (specificActions.Count > 0)
+                {
+                    if (needsLeadingSeparator)
+                    {
+                        items.AddSeparator();
+                    }
+                    items.AddActions(specificActions);
+                }
+                if (_sourceRepository != null)
+                {
+                    if (specificActions.Count > 0)
+                    {
+                        items.AddSeparator();
+                    }
+                    items.AddActions(
+                        new MenuAction("createBranch", "Create branch for task {0}".FormatInvariant(taskId), true, 
+                            () => CreateBranch(taskId)),
+                        new MenuAction("goToBranch", "Go to branch for task {0}".FormatInvariant(taskId), true,
+                            () => GoToBranchFor(taskId))
+                    );
+                }
+            }
+        }
+
+        private void CreateBranch(object taskId)
+        {
+            Debug.Assert(_sourceRepository != null);
+            _sourceRepository.CreateBranch(taskId);
+        }
+
+        private void GoToBranchFor(object taskId)
+        {
+            Debug.Assert(_sourceRepository != null);
+            var branches = _sourceRepository.LoadBranches();
+            var dataRow = branches.FindFirst("TaskID", taskId);
+            // TODO: create BranchBrowser, initialize it with dataRow["ID"] and push it to the container
+        }
+
+        private void taskGrid_ContextMenuNeeded(object sender, ContextMenuNeededEventArgs e)
+        {
+            var menu = BuildTaskActionMenu();
+            e.ContextMenu = menu;
+        }
+
+        private void taskGrid_RowInvoked(object sender, EventArgs e)
+        {
+            InvokeDefaultTaskGridAction();
+        }
+
+        private ContextMenu BuildTaskActionMenu()
+        {
+            var menu = new ContextMenu();
+            AddTaskSpecificActions(menu.MenuItems, false);
+            return menu;
+        }
+
+        private void InvokeDefaultTaskGridAction()
+        {
+            var menu = BuildTaskActionMenu();
+            menu.MenuItems.InvokeFirstMenuItem();
+        }
+
+        #endregion
+
+        #region IHistoryItem Members
+
+        IHistoryContainer IHistoryItem.Container { get; set; }
+
+        public Control Control { get { return this; } }
+
+        public string Title { get { return "Tasks"; } }
+
+        public void Reload()
+        {
+            SwitchCurrentTab(true);
+        }
+        #endregion
+    }
+}
